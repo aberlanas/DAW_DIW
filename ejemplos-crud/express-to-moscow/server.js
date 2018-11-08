19f09b8ecefd253e598815652c93d44657e6b9f3
Angel Berlanas Vicente <angel.berlanas@gmail.com>
2018-11-08 08:45:37 +0100

    Updated

--- ejemplos-crud/express-to-moscow/server.js
+++ ejemplos-crud/express-to-moscow/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const path = require('path');
 const app = express();
 
 // Utilizaremos body-parser para "parsear lo que nos pidan"
@@ -35,14 +35,11 @@ app.get('/',(req,res)=>{
     res.json({"message":"Parte Backend de nuestro programa"});
 });
 
+// Paginas publicas (estaticas)
+app.use(express.static(path.join(__dirname, 'public')));
+
 // Require Investigadores routes
 require('./app/routes/investigador.routes.js')(app);
-
-
-
-
-
-
 
 
 // Escuchemos en un puerto
