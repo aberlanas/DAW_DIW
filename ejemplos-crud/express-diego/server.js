96118a8ffcc99a3e97c18f059d83d3d725d317e5
aberlanas <angel.berlanas@gmail.com>
2018-11-13 08:57:33 +0100

    Actualizado el Miniserver

--- ejemplos-crud/express-diego/server.js
+++ ejemplos-crud/express-diego/server.js
@@ -7,5 +7,5 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Escuchemos en un puerto
 app.listen(3000,() => {
-    console.log(" * Server de Diego UP and Running en http://localhost:3000");
+    console.log(" * Miniserver UP and Running en http://localhost:3000");
 });
