9b37b5055741df666d4258b0ba23643acc6a64dd
Angel Berlanas Vicente <angel.berlanas@gmail.com>
2019-11-03 22:33:33 +0100

    + Preparando

--- Unidad02_CSS/Practicas/Tarea_12/script.js
+++ Unidad02_CSS/Practicas/Tarea_12/script.js
@@ -1,33 +1,7 @@
-function resize(e){
-    e.target.classList.add("todo");
-}
+/* 
+ 
+    ^(;,;)^ : Fragmento perdido
 
-function evolve(e){
-    console.log("evolve");
-    e.target.classList.add("evoluciona");
-    e.target.addEventListener("click",desevolve);
-}
-
-function desevolve(e){
-    e.target.classList.remove("evoluciona");
-    e.target.addEventListener("click",ultimate);
-}
-
-function ultimate(e){
-    e.target.classList.add("ultimate");
-}
-
-function createBox(){
-    const newBox = document.createElement("box");
-    newBox.addEventListener("click",evolve);
-    newBox.addEventListener("mouseOver",resize);
-    document.querySelector("container").appendChild(newBox);
-}
-
-
-function init(){
-    document.querySelector("button").addEventListener("click",createBox);
-}
-
+*/
 
 window.onload=init;
