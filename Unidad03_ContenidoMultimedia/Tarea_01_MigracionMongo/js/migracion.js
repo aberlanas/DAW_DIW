823149ebf1ebfd80b1ce377c759baebe4bb52709
Angel Berlanas Vicente <angel.berlanas@gmail.com>
2019-11-17 20:17:21 +0100

    + Tarea UD03

---
+++ Unidad03_ContenidoMultimedia/Tarea_01_MigracionMongo/js/migracion.js
@@ -0,0 +1,31 @@
+/*
+
+This Script is licensed under GPL v3 or higher
+
+Author: Angel Berlanas Vicente
+email : <berlanas_ang@gva.es>
+
+*/
+
+/*
+
+FUNCIONES PERDIDAS
+^(;,;)^
+
+*/
+
+function startMigration(){
+
+    // Fragmentos perdidos
+    // ^(;,;)^
+}
+
+function init(){
+    console.info(" * Init envirnoment ");
+
+    // Set click function on button
+    document.querySelector("button").addEventListener("click",startMigration);
+}
+
+// Init the environment when all is ready
+window.onload=init;
