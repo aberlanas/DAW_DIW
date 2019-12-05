b5afc447cc87aead21bc7c06d577c610b5854ef7
Angel Berlanas Vicente <angel.berlanas@gmail.com>
2019-12-05 10:03:25 +0100

    Algunos Cambios

--- Unidad04_FormulariosInteractividad/Tarea01_NodeJSPlusSass/public/js/buscar.js
+++ Unidad04_FormulariosInteractividad/Tarea01_NodeJSPlusSass/public/js/buscar.js
@@ -1,4 +1,31 @@
 
 const fuentesUrl = "http://mapas.valencia.es/lanzadera/opendata/cia_fuentes/JSON";
 
-console.log("Hola");
+const fetchPromesa = fetch(fuentesUrl);
+
+function filtroLetra(elemento){
+    //console.log(elemento);
+    let letra=document.querySelector(`input[name="calle"]`).value;
+    return elemento.properties.calle.startsWith(letra);
+}
+
+function buscar(){
+
+    fetchPromesa.then(response => {
+	return response.json();
+    }).then(respuesta =>{
+	const resultado=respuesta.features.filter(filtroLetra);
+	console.log(resultado);
+    });
+
+}
+
+function init(){
+
+    document.querySelector(`input[type="button"]`).addEventListener("click",buscar);
+	
+}
+
+
+window.onload=init;
+    
