ef70942248eb91452dedd9d0c30720faa1d088e2
Angel Berlanas Vicente <angel.berlanas@gmail.com>
2019-12-09 15:14:17 +0100

    Updated

--- Unidad04_FormulariosInteractividad/Tarea01_NodeJSPlusSass/public/js/buscar.js
+++ Unidad04_FormulariosInteractividad/Tarea01_NodeJSPlusSass/public/js/buscar.js
@@ -1,46 +1,49 @@
+// Este codigo esta TAN BIEN HECHO QUE ES AUTOCOMENTADO
+// ESTA PEPO.
 
 const fuentesUrl = "http://mapas.valencia.es/lanzadera/opendata/cia_fuentes/JSON";
 
-
-function filtroLetra(elemento){
-    //console.log(elemento);
-    let letra=document.querySelector(`input[name="calle"]`).value;
-    return elemento.properties.calle.startsWith(letra);
+function filtroLetra(elemento) {
+	//console.log(elemento);
+	let letra = document.querySelector(`input[name="calle"]`).value;
+	return elemento.properties.calle.startsWith(letra);
 }
 
-function toUpp(){
-    document.querySelector(`input[name="calle"]`).value=document.querySelector(`input[name="calle"]`).value.toUpperCase();
+function toUpp() {
+	document.querySelector(`input[name="calle"]`).value = document.querySelector(`input[name="calle"]`).value.toUpperCase();
 }
 
-function buscar(){
+function buscar() {
 
-    const fetchPromesa = fetch(fuentesUrl);
+	const fetchPromesa = fetch(fuentesUrl);
 
-    fetchPromesa.then(response => {
-	return response.json();
-    }).then(respuesta =>{
-	const resultado=respuesta.features.filter(filtroLetra);
-	console.log(resultado);
+	fetchPromesa.then(response => {
+		return response.json();
+	}).then(respuesta => {
 
-	let listado=document.createElement("ul");
-	resultado.forEach(fuente=>{
-	    console.log(fuente.properties.calle);
-	    let calleli=document.createElement("li");
-	    calleli.innerHTML=fuente.properties.calle+" -- ["+fuente.geometry.coordinates+"]";
-	    listado.appendChild(calleli);	    
+		console.log(respuesta);
+		const resultado = respuesta.features.filter(filtroLetra);
+		console.log(resultado);
+
+		let listado = document.createElement("ul");
+		resultado.forEach(fuente => {
+			console.log(fuente.properties.calle);
+			let calleli = document.createElement("li");
+			calleli.innerHTML = fuente.properties.calle + " -- [" + fuente.geometry.coordinates + "]";
+			listado.appendChild(calleli);
+		});
+		document.querySelector(".resultados").innerHTML = "";
+		document.querySelector(".resultados").appendChild(listado);
 	});
-	document.querySelector(".resultados").innerHTML="";
-	document.querySelector(".resultados").appendChild(listado);
-    });
 
 }
 
-function init(){
+function init() {
 
-    document.querySelector(`input[type="button"]`).addEventListener("click",buscar);
-    document.querySelector(`input[type="text"]`).addEventListener("input",toUpp);
+	document.querySelector(`input[type="button"]`).addEventListener("click", buscar);
+	document.querySelector(`input[type="text"]`).addEventListener("input", toUpp);
 }
 
 
-window.onload=init;
-    
+window.onload = init;
+
