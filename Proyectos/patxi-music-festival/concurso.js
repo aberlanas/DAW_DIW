3b98d333ccc22807e6e5f2142550d91dca936967
Angel Berlanas Vicente <angel.berlanas@gmail.com>
2019-12-14 17:32:44 +0100

    Working in a Christmas Festival

--- Proyectos/patxi-music-festival/concurso.js
+++ Proyectos/patxi-music-festival/concurso.js
@@ -3,8 +3,12 @@
 // author : Angel Berlanas Vicente
 // email  : <berlanas_ang@gva.es>
 
+let puntRojo = 0;
+let puntVerde = 0;
+let puntAmarillo = 0;
+let puntAzul = 0;
 
-function zoomIn(){
+function zoomIn() {
     console.log(this);
     let textoEscondido = this.querySelector(".escondido");
     console.log(textoEscondido);
@@ -21,39 +25,73 @@ function replenishBoard(json) {
 
         item.innerHTML = cancion.numero;
 
-	let escondidos = document.createElement("p");
-	escondidos.classList.toggle("escondido");
-	escondidos.innerHTML=cancion.cancion+"<br>"+cancion.interprete+"<br>"+cancion.alumno;
-	item.appendChild(escondidos);
-	
+        let escondidos = document.createElement("p");
+        escondidos.classList.toggle("escondido");
+        escondidos.innerHTML = cancion.cancion + "<br>" + cancion.interprete + "<br>" + cancion.alumno;
+        item.appendChild(escondidos);
+
         item.dataset.cancion = cancion.cancion;
-	item.dataset.interprete = cancion.interprete;
-	
-	
+        item.dataset.interprete = cancion.interprete;
+
+
         item.dataset.corto = 10;
 
 
-	// Zona de eventos
-	item.addEventListener("mouseover",zoomIn);
-	
-	
+        // Zona de eventos
+        item.addEventListener("mouseover", zoomIn);
+
+
         document.querySelector("canciones").appendChild(item);
 
     });
 }
 
 
-function buildBoard(){
-fetch('canciones.json')
-    .then(response=>response.json())
-    .then(auxjson => {
-	json=auxjson;
-	replenishBoard(json);
-    });
+function buildBoard() {
+    fetch('canciones.json')
+        .then(response => response.json())
+        .then(auxjson => {
+            json = auxjson;
+            replenishBoard(json);
+        });
 }
+
+
+
+function snow(num, speed) {
+    if (num > 0) {
+        setTimeout(function() {
+            $('#drop_' + randomInt(1, 250)).addClass('animate');
+            num--;
+            snow(num, speed);
+        }, speed);
+    }
+};
+
+function snowDrop(num, position) {
+    if (num > 0) {
+        var drop = '<div class="drop snow" id="drop_' + num + '"></div>';
+
+        $('body').append(drop);
+        $('#drop_' + num).css('left', position);
+        num--;
+        snowDrop(num, randomInt(60, 1280));
+    }
+};
+
+function randomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+
 
 function init() {
     buildBoard();
+    buildScore();
+
+    //make snow
+    snowDrop(150, randomInt(1035, 1280));
+    snow(150, 150);
 }
 
 window.onload = init;
