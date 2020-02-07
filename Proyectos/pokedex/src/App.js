127f14c9dedb2fcba8d911a1fd4114e7a5cb6d61
Angel Berlanas Vicente <angel.berlanas@gmail.com>
2020-02-07 21:11:25 +0100

    + Working API http + CORS

--- Proyectos/pokedex/src/App.js
+++ Proyectos/pokedex/src/App.js
@@ -1,55 +1,61 @@
-import React , {Component} from 'react';
+import React, { Component } from 'react';
 import logo from './imgs/pokeapi_256.png';
 import './App.css';
 
 
-const API = 'https://pokeapi.co/api/v2/';
+const API = 'https://localhost:3333/api/v2/';
 
 class Busqueda extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      pokemons: [],
-      query: '',
-    };
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            pokemons: [],
+            query: '',
+        };
+    }
 
-  componentDidMount() {
-    //First method to be called after components mount
-    //fetch the data from the server for the suggestion
-    fetch(`${API}/pokemon/`)
-      .then(res => res.json())
-      .then(json => {
-        const { results: pokemons } = json;
-        console.log(json);
-        this.setState({ pokemons });
-        //setting the data in the films state
-      });
-  }
+    componentDidMount() {
+        //First method to be called after components mount
+        //fetch the data from the server for the suggestion
+        fetch(`${API}/pokemon/`)
+            .then(res => res.json())
+            .then(json => {
+                const { results: pokemons } = json;
+                console.log(json);
+                this.setState({ pokemons });
+                //setting the data in the films state
+            });
+    }
 
-  render(){
-    return (<div className="Patata">
-      "Angel"
-    </div>);
-  }
-
-
+    render() {
+        return ( < div className = "Patata" >
+            "Angel" <
+            /div>);
+        }
 
 
-}
 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-      <Busqueda>
+    }
 
-      </Busqueda>
-    </div>
-  );
-}
 
-export default App;
+    function App() {
+        return ( <
+            div className = "App" >
+            <
+            header className = "App-header" >
+            <
+            img src = { logo }
+            className = "App-logo"
+            alt = "logo" / >
+            <
+            /header> <
+            Busqueda >
+
+            <
+            /Busqueda> <
+            /div>
+        );
+    }
+
+    export default App;
--- Proyectos/pokeproxy/server.js
+++ Proyectos/pokeproxy/server.js
@@ -1,15 +1,15 @@
 var express = require('express');
 var proxy = require('http-proxy-middleware');
- 
+
 var app = express();
- 
+
 app.use(
-  '/api/v2/',
+    '/api',
     proxy({
-	target: 'https://pokeapi.co/api/v2/',
-	secure:false,
-	changeOrigin: true,
-	logLevel: 'debug'
+        target: 'https://pokeapi.co/',
+        //secure: false,
+        changeOrigin: true,
+        logLevel: 'debug'
     })
 );
 app.listen(3333);
