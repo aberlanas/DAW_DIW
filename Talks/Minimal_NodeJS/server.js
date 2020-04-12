fc40ae2f6a02ef0a992ef725922adf255a611dc0
Angel Berlanas Vicente <angel.berlanas@gmail.com>
2020-04-12 12:20:20 +0200

    Updated

--- Talks/Minimal_NodeJS/server.js
+++ Talks/Minimal_NodeJS/server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const app = express();
-const bodyParser = require('body-parser');
 
 app.all('/*', function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
@@ -13,9 +12,10 @@ var port = process.env.PORT || 3000;        // set our port
 var router = express.Router();    
 
 app.get('/api/registraUsuarios', function (req, res) {
+  console.log(req);
+  console.log(" Petición GET ");
     res.send({usuario:"Angel"});
-    console.log(req);
-    console.log(" Petición GET ");
+
   });
 
 app.use('/static', express.static(__dirname + '/public'));
